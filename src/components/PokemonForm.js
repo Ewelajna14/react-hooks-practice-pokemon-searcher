fc8a10d6589e9f500f2b96c166a5d174ac8d47dd
Ewelina <ekowalczyk1994@gmail.com>
2021-11-20 13:18:42 -0600

    Done.

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -1,29 +1,80 @@
-import React from "react";
+import React, {useState} from "react";
 import { Form } from "semantic-ui-react";
 
-function PokemonForm() {
+
+function PokemonForm({onNewPokemon}) {
+
+const[name, setName]= useState("")
+const[hp, sethp]= useState("")
+const[front, setFront]= useState("")
+const[back, setBack]= useState("")
+
+function handleName(event){
+setName(event.target.value)
+}
+
+function handleHp(event){
+sethp(event.target.value)
+}
+
+function handleFront(event){
+setFront(event.target.value)
+}
+
+function handleBack(event){
+setBack(event.target.value)
+}
+
+function handleSubmit(event){
+event.preventDefault()
+const newPokemon ={
+  name:name,
+  hp:hp,
+  sprites:{
+  front:front,
+  back:back
+  }
+}
+fetch("http://localhost:3001/pokemon", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newPokemon),
+  })
+    .then((r) => r.json())
+    .then((newPokemon) => onNewPokemon(newPokemon));
+
+setName("");
+sethp("")
+setFront("")
+setBack("")
+
+}
+
+
   return (
     <div>
       <h3>Add a Pokemon!</h3>
       <Form
-        onSubmit={() => {
-          console.log("submitting form...");
-        }}
+        onSubmit={handleSubmit}
       >
         <Form.Group widths="equal">
-          <Form.Input fluid label="Name" placeholder="Name" name="name" />
-          <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-          <Form.Input
+          <Form.Input onChange={handleName} fluid label="Name" placeholder="Name" name="name" value={name}/>
+          <Form.Input onChange={handleHp} fluid label="hp" placeholder="hp" name="hp" value={hp}/>
+          <Form.Input onChange={handleFront}
             fluid
             label="Front Image URL"
             placeholder="url"
             name="frontUrl"
+            value={front}
           />
-          <Form.Input
+          <Form.Input onChange={handleBack}
             fluid
             label="Back Image URL"
             placeholder="url"
             name="backUrl"
+            value={back}
           />
         </Form.Group>
         <Form.Button>Submit</Form.Button>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-function Search() {
+function Search({search, setSearch}) {
+  
+
+ function handleChange(event){
+  setSearch(event.target.value)
+ }
+
   return (
-    <div className="ui search">
+    <div  className="ui search">
       <div className="ui icon input">
-        <input className="prompt" />
+        <input onChange={handleChange} className="prompt" value={search} />
         <i className="search icon" />
       </div>
     </div>
